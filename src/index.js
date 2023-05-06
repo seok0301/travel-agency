2943d30ab30e360bedaa303b61a0e01f1ecc1180
seok0301 <seok990301@gmail.com>
2023-05-07 00:20:39 +0900

    add BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/travel-agency">
     <CssBaseline />
     <App />
   </BrowserRouter>
